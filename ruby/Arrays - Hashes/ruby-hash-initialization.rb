b2860e149b651b362ab4207951d3edba4ff02d33
LikeaNachozBR <30394349+LikeaNachozBR@users.noreply.github.com>
2018-01-04 22:55:01 -0300

    Create ruby-hash-initialization.rb

---
+++ ruby/Arrays - Hashes/ruby-hash-initialization.rb
@@ -0,0 +1,10 @@
+# Initialize 3 variables here as explained in the problem statement
+empty_hash = Hash.new
+
+default_hash = {}
+default_hash.default = 1
+=begin
+"simmy", 100  
+"vivmbbs",200
+=end
+hackerrank = {'simmy' => 100, 'vivmbbs' => 200}
